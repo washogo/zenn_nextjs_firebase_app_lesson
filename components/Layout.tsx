85f3ee36dee192cd51343f1681f0068858946f8f
washogo <aiz4338150@gmail.com>
2022-02-26 11:21:06 +0900

    Finish chapter 17

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,8 +1,23 @@
+import Head from "next/head";
 import { ToastContainer } from "react-toastify";
 
 export default function Layout({ children }) {
+  const title = "My質問回答サービス";
+  const description = "質問と回答を行えるサービスです。";
+
   return (
     <div>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" key="description" content={description} />
+        <meta property="og:title" key="ogTItle" content={title} />
+        <meta property="og:site_name" key="ogSiteName" content={title} />
+        <meta
+          property="og:description"
+          key="ogDescription"
+          content={description}
+        />
+      </Head>
       <nav
         className="navbar navbar-expand-lg navbar-light mb-3"
         style={{ backgroundColor: "#e3f2fd" }}
